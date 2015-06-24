c654156afa124dafa0b709f06198cf0bd104ea69
杨育星 <yangyuxing@yangyuxing-2.local>
2015-06-24 14:28:00 +0800

    gengxin daima

--- langchao/controllers/search.php
+++ langchao/controllers/search.php
@@ -1325,17 +1325,17 @@ class Search extends MY_Controller {
             $arrive = False;
             $back = False;
         }elseif (in_array($arrive_date, $h_weekend_list) && !in_array($back_date, $h_weekend_list)){
-            $arrive_tmp = strtotime($arrive_date." 00:00:00") + (3600*24) -strtotime($value['arrive_time']);
+            $arrive_tmp = strtotime($arrive_date." 00:00:00") + (3600*24) -strtotime($work_order['arrive_time']);
             list($arrive_int_tmp,$arrive_less_tmp) = $this->get_time_format($arrive_tmp);
             $weekend_more = $weekend_more+$arrive_int_tmp+$arrive_less_tmp;
             $arrive = False;
         }elseif (!in_array($arrive_date, $h_weekend_list) && in_array($back_date, $h_weekend_list)){
-            $back_tmp = strtotime($value['back_time']) - strtotime($back_date." 00:00:00");
+            $back_tmp = strtotime($work_order['back_time']) - strtotime($back_date." 00:00:00");
             list($back_int_tmp,$back_less_tmp) = $this->get_time_format($back_tmp);
             $weekend_more = $weekend_more+$back_int_tmp+$back_less_tmp;
             $back = False;
         }elseif (in_array($arrive_date, $h_weekend_list) && in_array($back_date, $h_weekend_list)){
-            $back_tmp = strtotime($value['back_time']) - strtotime($value['arrive_time']);
+            $back_tmp = strtotime($work_order['back_time']) - strtotime($work_order['arrive_time']);
             list($back_int_tmp,$back_less_tmp) = $this->get_time_format($back_tmp);
             $weekend_more = $weekend_more+$back_int_tmp+$back_less_tmp;
             $arrive = False;
